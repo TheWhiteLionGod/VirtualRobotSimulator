bbd09d0b58401c1883d63fedde184cb890247f38
Aryan Agarwal <aaryan12jul@gmail.com>
2025-08-23 13:02:35 -0400

    Converted Virtual Robot to use Trajectory Sequence
    Known Bugs:
    Anything Related to Turning Fails

--- TeamCode/src/org/firstinspires/ftc/teamcode/RoadRunnerTest.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/RoadRunnerTest.java
@@ -6,33 +6,40 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+import java.util.ArrayList;
 
 @TeleOp(name = "RoadRunnerTest", group = "FTC2025")
 public class RoadRunnerTest extends LinearOpMode {
-    @Override
-    public void runOpMode() {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    SampleMecanumDrive drive;
+    ArrayList<TrajectorySequence> trajectories;
+    public void configure() {
+        drive = new SampleMecanumDrive(hardwareMap);
         Pose2d start_pos = new Pose2d(0, 0,0);
         drive.setPoseEstimate(start_pos);
+        trajectories = new ArrayList<>();
 
-        Trajectory traj = drive.trajectoryBuilder(start_pos)
-                .lineToConstantHeading(new Vector2d(12, 0))
+        TrajectorySequence traj = drive.trajectorySequenceBuilder(start_pos)
+                .splineToConstantHeading(new Vector2d(12, 0), 0)
+                .turn(Math.toRadians(45))
+                .splineToConstantHeading(new Vector2d(-5, -5), 0)
                 .build();
 
-        Trajectory traj2 = drive.trajectoryBuilder(traj.end())
-                .splineToConstantHeading(new Vector2d(-5, -12), 0)
-                .build();
+        trajectories.add(traj);
+    }
 
+    @Override
+    public void runOpMode() {
+        configure();
         waitForStart();
 
         if (opModeIsActive()) {
-            drive.followTrajectory(traj);
-            System.out.println(traj.end());
-            System.out.println(drive.getPoseEstimate());
-
-            drive.followTrajectory(traj2);
-            System.out.println(traj2.end());
-            System.out.println(drive.getPoseEstimate());
+            for (TrajectorySequence test_traj : trajectories) {
+                drive.followTrajectorySequence(test_traj);
+                System.out.println(test_traj.end());
+                System.out.println(drive.getPoseEstimate());
+            }
         }
     }
 }
